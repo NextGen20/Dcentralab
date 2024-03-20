780bd52cf34d05539abf2d3b2de82005840a8bdf
NextGen20 <71230554+NextGen20@users.noreply.github.com>
2024-03-20 21:39:31 +0200

    Update var.tf

--- Terraform/var.tf
+++ Terraform/var.tf
@@ -1,1 +1,28 @@
 
+variable "ec2_instance_type" {
+  default     = "t2.micro"
+}
+
+variable "rds_instance_type" {
+  default     = "db.t2.micro"
+}
+
+variable "rds_allocated_storage" {
+  default     = 20
+}
+variable "ami_id" {
+  type        = string
+  default = "ami-0d7a109bf30624c99"
+}
+variable "aws_region" {
+  default     = "us-east-1"
+}
+variable "db_username" {
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  type        = string
+  sensitive   = true
+}
